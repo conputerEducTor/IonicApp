43da264fbce8fb7253d63e9136095b9812667c5d
conputerEducTor <aryanirmal134@gmail.com>
2022-09-20 19:47:35 +0530

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,20 @@ const routes: Routes = [
   
   {
     path: '',
-    redirectTo: '/welcome',
+    redirectTo: '/login',
     pathMatch: 'full'
   },
   {
     path: 'welcome',
     loadChildren: () => import('./pages/welcome/welcome.module').then( m => m.WelcomePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./pages/signup/signup.module').then( m => m.SignupPageModule)
   },
 ];
 
